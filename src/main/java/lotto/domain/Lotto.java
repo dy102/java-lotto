5331ea35153a1ae78eb6256840d9d84e3c7b661c
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-08 00:09:03 +0900

    당첨 번호 입력받기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,12 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
-
 public class Lotto {
     private final List<Integer> numbers;
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -13,6 +16,14 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+        for(int i = 0; i < numbers.size(); i++){
+            int box = numbers.get(i);
+            numbers.set(i,0);
+            if(numbers.contains(box)){
+                throw new IllegalArgumentException();
+            }
+            numbers.set(i,box);
         }
     }
 
